c86951c51a2c6fde8b11067f9939a6f3d0c09fdc
Zakaria96 <zakarialavoro@gmail.com>
2023-04-26 14:49:23 +0200

    test js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// VARIABILI 
+
+
+// chiedi all’utente il suo nome
+
+
+// chiedi all’utente il suo cognome
+
+
+// chiedi all’utente il suo colore preferito
+
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+
+// DEBUG 
+document.getElementById("test").innerHTML= "ciao gianni"
