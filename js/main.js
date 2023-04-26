94832c8a3b0cdbba6e7234671e6624f32076391b
Zakaria96 <zakarialavoro@gmail.com>
2023-04-26 15:22:42 +0200

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -2,20 +2,22 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 // VARIABILI 
-
+let nameuser, lastNameUser, favoriteColor;
 
 // chiedi all’utente il suo nome
-
+nameuser = prompt("Inserisci il tuo nome");
+console.log(nameuser);
 
 // chiedi all’utente il suo cognome
-
+lastNameUser = prompt("Inserisci il tuo cognome");
+console.log(lastNameUser);
 
 // chiedi all’utente il suo colore preferito
-
+favoriteColor = prompt("Inserisci il tuo colore preferito");
+console.log(favoriteColor);
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
-
-
+document.getElementById("password").innerHTML=("Password consigliata:") + nameuser + lastNameUser + favoriteColor + 21 ;
 
 // DEBUG 
-document.getElementById("test").innerHTML= "ciao gianni"
+// document.getElementById("test").innerHTML= "ciao gianni"
